585fb6a8083626245f6507f79eb64fc6c8aded71
zodiacsoulmate <zodiacsoulmate@gmail.com>
2014-03-29 16:45:38 -0400

    FINAL hackWATERLOOOOOOOOOOOOOOOOOOOOOOOO MichaelLLLLLLLLL

--- public/js/controllers/PhoneCtrl.js
+++ public/js/controllers/PhoneCtrl.js
@@ -1,8 +1,15 @@
-angular.module('PhoneCtrl', []).controller('PhoneController', function($scope, $http) {
+angular.module('PhoneCtrl', []).controller('PhoneController', function($scope) {
 
 	$scope.tagline = 'The square root of life is pi!';	
-	console.log('switched to phone view');
-	getPhonelist($scope, $http);
-
+	
 	window.setTimeout(takeback,1000);
+
+//	$scope.linkScrolls = function() {
+//		jQuery('.leftseg').scroll(function(){
+//	    	jQuery('.leftseg').scrollTop(jQuery('.rightseg').scrollTop());    
+//		});
+//		jQuery('.rightseg').scroll(function(){
+//	    	jQuery('.rightseg').scrollTop(jQuery('.leftseg').scrollTop());    
+//		})
+//	}
 });
